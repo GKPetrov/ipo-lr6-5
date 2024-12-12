ce8ab237bfdc2d45cc67f98c3c44af344cb2645a
Gleb <glebpetrof2007@gmail.com>
2024-12-12 10:26:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ print(num_list)
 zero = 0
 positive = 0
 negative = 0
-for i in range(len(num_list)):
-    if num_list[i] > 0:
+for num in num_list:
+    if num > 0:
         positive += 1
-    elif num_list[i] < 0:
+    elif num < 0:
         negative += 1
-    elif num_list[i] == 0:
+    elif num == 0:
         zero+=1
-print(f'Количество нулей ={zero}. Процент от общего количества {zero/len(a)*100}')
-print(f'Количество положительных ={positive}. Процент от общего количества {positive/len(a)*100}')
-print(f'Количество отрицательных ={negative}. Процент от общего количества {negative/len(a)*100}')
-print(f'Максимальное значение={max(a)}')
-print(f'Минимальное значение={min(a)}')
+print(f'Количество нулей ={zero}. Процент от общего количества {zero/len(num_list)*100}')
+print(f'Количество положительных ={positive}. Процент от общего количества {positive/len(num_list)*100}')
+print(f'Количество отрицательных ={negative}. Процент от общего количества {negative/len(num_list)*100}')
+print(f'Максимальное значение={max(num_list)}')
+print(f'Минимальное значение={min(num_list)}')
